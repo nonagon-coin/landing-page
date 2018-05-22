922c6ac4f48bf817a8b68a0bfe22d03814924673
nonagon-coin <judeno@nonagon.xyz>
2018-05-22 18:36:42 +0900

    a

--- project.clj
+++ project.clj
@@ -4,8 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  
-  
   :min-lein-version "2.7.1"
 
   :dependencies [[org.clojure/clojure "1.9.0"]
